b17566fc954b507d51eb22f3abf11f6ab16433ea
Elena-Abreu <ena3@njit.edu>
2024-11-12 11:02:33 -0500

    ===

--- scriptOne.js
+++ scriptOne.js
@@ -1,5 +1,5 @@
 let a = 100; 
-let b = 20;  
+let b = 100;  
 
 // // a +=10; ==> a = a + 10
 
@@ -9,5 +9,6 @@ let b = 20;
 
 
 //a==b means it will check if both values are the same , so true or false 
-//a!=b checks if both values are different 
-console.log(a != b);
+//a!=b checks if both values are different  
+//a===b compares total and datatype , if they are both the same like a=b then it will compare the datatype being the same (numbers..?) so if one has quotes its false
+console.log(a === b);
