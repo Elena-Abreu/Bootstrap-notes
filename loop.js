008750c4bb9f60c6ad04d97ead68cad65249d25e
Elena-Abreu <ena3@njit.edu>
2024-11-19 10:26:59 -0500

    character kength and letter at certain point from  begginning

--- loop.js
+++ loop.js
@@ -6,10 +6,22 @@ console.log("hello");
 
 //can use i j k or whatever as variable for loop and then initialize it to something like =1; alternative to if (a<b) {}
 //check condition i<=5; if true code wll run 
-//then i++ will be implemented, which means i=1 becomes i=2 (which is still true)
+//then i++ will be implemented, which means i=1 becomes i=2 (which is still true for 5 loops)
+//i++ is i=i+1 
 for(let i=1; i <=5; i++){ 
 //block of code 
 //line 1   
 console.log("Hello");
 
-}
+} 
+
+//find out given characters in string (x.length)
+//starts from first letter in first element in sequence, at 0 index
+let firstName = "Harry Potter"
+
+console.log(firstName.length) 
+
+//find character at a given index, 0 is the first letter
+let lastName = "Elena Abreu"
+
+console.log(lastName[0])
